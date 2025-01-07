8a7c69c0f7eec34f43cf83cfeb32462efccb00a5
kobit <kobit@just-4.dev>
2025-01-07 15:34:08 -0700

    Fixed copyright header

--- shared/src/commonMain/kotlin/org/latinpray/data/Prayer.kt
+++ shared/src/commonMain/kotlin/org/latinpray/data/Prayer.kt
@@ -13,36 +13,6 @@
  *  If not, see http://www.gnu.org/licenses/.
  */
 
-/*
- * This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Affero General Public License as published by
- *  the Free Software Foundation, version 3 of the License.
- *
- *   This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Affero General Public License for more details.
- *
- *  You should have received a copy of the GNU Affero General Public License
- *  along with this program. Look for COPYING file in the top folder.
- *  If not, see http://www.gnu.org/licenses/.
- */
-
-/*
- * This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Affero General Public License as published by
- *  the Free Software Foundation, version 3 of the License.
- *
- *   This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Affero General Public License for more details.
- *
- *  You should have received a copy of the GNU Affero General Public License
- *  along with this program. Look for COPYING file in the top folder.
- *  If not, see http://www.gnu.org/licenses/.
- */
-
 package org.latinpray.data
 
 import kotlinx.serialization.SerialName
@@ -60,8 +30,8 @@ data class BasicPrayer(
     val title: String,
     val lang: String,
     val language: String,
-    val date: String? = null,
     val lines: List<String?>,
+    val dates: List<String>? = null,
     val tags: Set<String>? = null,
     val links: List<Link>? = null,
     val notes: String? = null
