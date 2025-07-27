99bdd123796029de62807a67731765a1bd8cda10
kobit <kobit@just-4.dev>
2025-07-27 01:34:39 -0600

    Added a message when a prayer is added or removed from daily or favorite prayers

--- shared/src/commonMain/kotlin/org/latinpray/ui/PrayerDetailsScreen.kt
+++ shared/src/commonMain/kotlin/org/latinpray/ui/PrayerDetailsScreen.kt
@@ -15,7 +15,14 @@
 
 package org.latinpray.ui
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -35,8 +42,6 @@ import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconToggleButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBackIos
 import androidx.compose.material.icons.automirrored.filled.FormatAlignLeft
@@ -46,11 +51,14 @@ import androidx.compose.material.icons.outlined.Cancel
 import androidx.compose.material.icons.outlined.Done
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconToggleButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
@@ -64,6 +72,7 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
@@ -71,6 +80,8 @@ import org.latinpray.data.Config
 import org.latinpray.data.Prayer
 import org.latinpray.data.PrayerIntention
 import org.latinpray.shared.Res
+import org.latinpray.shared.added_to_daily
+import org.latinpray.shared.added_to_favorites
 import org.latinpray.shared.align_end_24dp
 import org.latinpray.shared.align_start_24dp
 import org.latinpray.shared.bookmark_add
@@ -82,13 +93,15 @@ import org.latinpray.shared.intention_add
 import org.latinpray.shared.intention_days
 import org.latinpray.shared.intention_text
 import org.latinpray.shared.intentions_title
+import org.latinpray.shared.removed_from_daily
+import org.latinpray.shared.removed_from_favorites
 import org.latinpray.theme.Gray300
 import org.latinpray.theme.Green300
 import org.latinpray.theme.Green900
 import org.latinpray.theme.Orange900
+import org.latinpray.util.DisplayLang
 import org.latinpray.util.findNextActiveIntention
 import org.latinpray.util.getCurrentIntention
-import org.latinpray.util.DisplayLang
 
 @Composable
 fun IntentionsForm(item: PrayerIntention) {
@@ -105,9 +118,9 @@ fun IntentionsForm(item: PrayerIntention) {
             .padding(horizontal = 8.dp)
             .border(1.dp, color = color, shape = MaterialTheme.shapes.medium),
     ) {
-        Column (
+        Column(
             modifier = Modifier.padding(8.dp),
-        ){
+        ) {
             OutlinedTextField(
                 value = text,
                 onValueChange = {
@@ -117,16 +130,20 @@ fun IntentionsForm(item: PrayerIntention) {
                 readOnly = false,
                 minLines = 2,
                 maxLines = 2,
-                label = { Text( text = stringResource(Res.string.intention_text),
-                    style = MaterialTheme.typography.bodySmall) },
+                label = {
+                    Text(
+                        text = stringResource(Res.string.intention_text),
+                        style = MaterialTheme.typography.bodySmall
+                    )
+                },
                 modifier = Modifier
                     .fillMaxWidth()
             )
             Row() {
-                Column (
+                Column(
                     modifier = Modifier.padding(end = 16.dp),
                     horizontalAlignment = Alignment.CenterHorizontally
-                ){
+                ) {
                     Text(
                         text = stringResource(Res.string.intention_active),
                     )
@@ -158,14 +175,15 @@ fun IntentionsForm(item: PrayerIntention) {
 
 @Composable
 fun IntentionsDialog(
-    onDismissRequest : () -> Unit = {},
-    onOkRequest : (List<PrayerIntention>) -> Unit = {},
+    onDismissRequest: () -> Unit = {},
+    onOkRequest: (List<PrayerIntention>) -> Unit = {},
     prayerIntentions: List<PrayerIntention> = emptyList()
 ) {
     Dialog(
         onDismissRequest = onDismissRequest,
     ) {
-        val itemList = remember { mutableStateListOf<PrayerIntention>(*prayerIntentions.toTypedArray()) }
+        val itemList =
+            remember { mutableStateListOf<PrayerIntention>(*prayerIntentions.toTypedArray()) }
 
         // Your form content here (e.g., TextFields, Buttons)
         Box(
@@ -239,6 +257,62 @@ fun IntentionsDialog(
 
 }
 
+@Composable
+fun MessageDialog(
+    message: String,
+    show: Boolean = true,
+    onDismissRequest: () -> Unit = {},
+) {
+    var showMessage by remember { mutableStateOf(show) }
+    val scope = rememberCoroutineScope()
+    scope.launch {
+        delay(2000)
+        showMessage = false
+        onDismissRequest()
+    }
+    AnimatedVisibility(
+        enter = fadeIn() + scaleIn(),
+        exit = fadeOut() + scaleOut(),
+        visible = showMessage,
+    ) {
+        Dialog(
+            onDismissRequest = onDismissRequest,
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(color = MaterialTheme.colorScheme.surface)
+                    .padding(8.dp)
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    Text(
+                        text = message,
+                        textAlign = TextAlign.Center,
+                    )
+                    IconButton(
+                        onClick = {
+                            showMessage = false
+                            onDismissRequest()
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Outlined.Done,
+                            contentDescription = "Ok",
+                            tint = Green900,
+                            modifier = Modifier.size(30.dp)
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+}
+
 @OptIn(ExperimentalSharedTransitionApi::class, ExperimentalFoundationApi::class)
 @Composable
 fun PrayerDetailsScreen(
@@ -250,7 +324,7 @@ fun PrayerDetailsScreen(
     var prayer by remember { mutableStateOf(startPrayer) }
     val (fraction) = remember { mutableStateOf(0.25f) }
     var firstLang by remember { mutableStateOf(true) }
-    var displayLang by remember { mutableStateOf(DisplayLang.BOTH ) }
+    var displayLang by remember { mutableStateOf(DisplayLang.BOTH) }
     var daily by remember { mutableStateOf(config.dailyPrayers.contains(prayer.name)) }
     var favorite by remember { mutableStateOf(config.favorites.contains(prayer.name)) }
     val scope = rememberCoroutineScope()
@@ -260,9 +334,23 @@ fun PrayerDetailsScreen(
     var showDialog by remember { mutableStateOf(false) }
     var endProcessed by remember { mutableStateOf(false) }
 
-    var prayerIntentions by remember { mutableStateOf(config.loadIntentions(prayer) ) }
+    var prayerIntentions by remember { mutableStateOf(config.loadIntentions(prayer)) }
     var currentIntention by remember { mutableStateOf(getCurrentIntention(prayer, config)) }
-    //println("Current currentIntention: ${currentIntention?.toPropsString()}")
+
+    var dlgMessage by remember { mutableStateOf("") }
+    var showMessage by remember { mutableStateOf(false) }
+
+    val added_to_daily = stringResource(Res.string.added_to_daily)
+    val removed_from_daily = stringResource(Res.string.removed_from_daily)
+    val added_to_favorites = stringResource(Res.string.added_to_favorites)
+    val removed_from_favorites = stringResource(Res.string.removed_from_favorites)
+
+    if (showMessage) {
+        MessageDialog(
+            message = dlgMessage,
+            onDismissRequest = { showMessage = false }
+        )
+    }
 
     if (showDialog) {
         IntentionsDialog(
@@ -310,7 +398,7 @@ fun PrayerDetailsScreen(
             Row(
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.spacedBy(8.dp)
-                ) {
+            ) {
                 IconButton(
                     onClick = {
                         goBack()
@@ -355,6 +443,7 @@ fun PrayerDetailsScreen(
                                 modifier = Modifier.size(30.dp)
                             )
                         }
+
                         DisplayLang.SECOND -> {
                             Icon(
                                 painter = painterResource(Res.drawable.align_end_24dp),
@@ -363,6 +452,7 @@ fun PrayerDetailsScreen(
                                 modifier = Modifier.size(30.dp)
                             )
                         }
+
                         DisplayLang.BOTH -> {
                             Icon(
                                 imageVector = Icons.AutoMirrored.Filled.FormatAlignLeft,
@@ -380,9 +470,12 @@ fun PrayerDetailsScreen(
                         scope.launch {
                             if (daily) {
                                 config.addDailyPrayer(prayer.name)
+                                dlgMessage = added_to_daily
                             } else {
                                 config.removeDailyPrayer(prayer.name)
+                                dlgMessage = removed_from_daily
                             }
+                            showMessage = true
                         }
                     }
                 ) {
@@ -409,9 +502,12 @@ fun PrayerDetailsScreen(
                         scope.launch {
                             if (favorite) {
                                 config.addFavorite(prayer.name)
+                                dlgMessage = added_to_favorites
                             } else {
                                 config.removeFavorite(prayer.name)
+                                dlgMessage = removed_from_favorites
                             }
+                            showMessage = true
                         }
                     }
                 ) {
@@ -434,36 +530,36 @@ fun PrayerDetailsScreen(
             }
         }
         //key (totalNum, inrowNum) {
-            PrayerDetails(
-                displayLang = displayLang,
-                prayer = prayer,
-                config = config,
-                prayers = prayers,
-                endReachedCallbackPD = {
-                    if (endProcessed) return@PrayerDetails
-                    endProcessed = true
-                    //println("End reached start: ${prayer.name} - ${prayer.nums}, ${currentIntention}")
-                    config.incPrayerNum(prayer, prayerIntentions)
-                    totalNum = prayer.nums.totalNum
-                    inrowNum = prayer.nums.inrowNum
-                    //prayerIntentions = config.loadIntentions(prayer)
-                    currentIntention = getCurrentIntention(prayer, config)
-                    //println("End reached end: ${prayer.name} - ${prayer.nums}, ${currentIntention}")
-                },
-                intention = currentIntention,
-                prayerChangedCallback = { pr ->
-                    //println("Prayer changed start: ${prayer.name} - ${prayer.nums}, ${currentIntention}")
-                    prayer = pr
-                    daily = config.dailyPrayers.contains(prayer.name)
-                    favorite = config.favorites.contains(prayer.name)
-                    totalNum = prayer.nums.totalNum
-                    inrowNum = prayer.nums.inrowNum
-                    prayerIntentions = config.loadIntentions(prayer)
-                    currentIntention = getCurrentIntention(prayer, config)
-                    endProcessed = false
-                    //println("Prayer changed end: ${prayer.name} - ${prayer.nums}, ${currentIntention}")
-                }
-            )
+        PrayerDetails(
+            displayLang = displayLang,
+            prayer = prayer,
+            config = config,
+            prayers = prayers,
+            endReachedCallbackPD = {
+                if (endProcessed) return@PrayerDetails
+                endProcessed = true
+                //println("End reached start: ${prayer.name} - ${prayer.nums}, ${currentIntention}")
+                config.incPrayerNum(prayer, prayerIntentions)
+                totalNum = prayer.nums.totalNum
+                inrowNum = prayer.nums.inrowNum
+                //prayerIntentions = config.loadIntentions(prayer)
+                currentIntention = getCurrentIntention(prayer, config)
+                //println("End reached end: ${prayer.name} - ${prayer.nums}, ${currentIntention}")
+            },
+            intention = currentIntention,
+            prayerChangedCallback = { pr ->
+                //println("Prayer changed start: ${prayer.name} - ${prayer.nums}, ${currentIntention}")
+                prayer = pr
+                daily = config.dailyPrayers.contains(prayer.name)
+                favorite = config.favorites.contains(prayer.name)
+                totalNum = prayer.nums.totalNum
+                inrowNum = prayer.nums.inrowNum
+                prayerIntentions = config.loadIntentions(prayer)
+                currentIntention = getCurrentIntention(prayer, config)
+                endProcessed = false
+                //println("Prayer changed end: ${prayer.name} - ${prayer.nums}, ${currentIntention}")
+            }
+        )
         //}
     }
 }
